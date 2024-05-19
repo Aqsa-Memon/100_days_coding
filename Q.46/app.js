8f9d5cd7a3397a9d1f584ede9c0e1eae356313cd
Aqsa-Memon <muhammadzaidjati2@gmail.com>
2024-05-19 23:44:01 +0500

    coding

--- Q.46/app.js
+++
@@ -1,9 +0,0 @@
-var laptop = {
-    make: 'Dell',
-    model: 'I7 Gen 8',
-    year: '2021',
-    Describe: function () {
-        console.log("This laptop is a ".concat(this.year, " ").concat(this.make, " ").concat(this.model, ". "));
-    }
-};
-laptop.Describe();
