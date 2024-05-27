419df6de621fea44ded88d14de0cbf0eacffe2b0
Aqsa-Memon <muhammadzaidjati2@gmail.com>
2024-05-27 22:09:14 +0500

    coding

---
+++ Q.64/app.js
@@ -0,0 +1,5 @@
+function combineStringAndNumber(text, number) {
+    return text + number;
+}
+var combine = combineStringAndNumber("age:", 30);
+console.log(combine);
---
+++ Q.64/app.ts
@@ -0,0 +1,9 @@
+
+
+function combineStringAndNumber(text: string, number: number): string {
+ 
+ return text + number;
+}
+let combine = combineStringAndNumber("age:", 30);
+console.log(combine); 
+
