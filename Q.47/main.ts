f74f55e2554962ae799512d5eaf6a4c11b79463d
Aqsa-Memon <muhammadzaidjati2@gmail.com>
2024-05-31 23:47:00 +0500

    coding

--- Q.47/main.ts
+++
@@ -1,8 +0,0 @@
-let laptops = [
-    {make: 'Dell',model: 'XPS 15',year: '20201'},
-    {make: 'samsung',model: 'e 6350',year: '20201'},
-    {make: 'hp',model: 'xyz 15',year: '2019'},
-];
-let[laptop1,laptop2,laptop3] = laptops;
-console.log(laptop1);
-console.log(laptop2);
